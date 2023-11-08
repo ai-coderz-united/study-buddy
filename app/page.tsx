9932b14f5ba70d152d7e4f5626a2629335077194
Cole Brant <cbrant229@gmail.com>
2023-11-08 00:57:02 -0700

    started on the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import { LoginButton, LogoutButton } from '@/components/LoginButtons'
 import { SessionLogger } from '@/components/SessionLogger'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
+import { Navbar } from '@/components/Navbar'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {session !== null ? <LogoutButton /> : <LoginButton />}
+    <main className='flex flex-col items-center justify-around min-h-screen'>
+      {session ? <Navbar session={session} /> : <Navbar />}
       <SessionLogger />
     </main>
   )
---
+++ components/Navbar.tsx
@@ -0,0 +1,88 @@
+'use client'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { LoginButton, LogoutButton } from "@/components/LoginButtons"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export const Navbar = ({session}:any) => {
+  if(!session) {
+    return (
+      <div className="w-full flex justify-between items-center" id="navbar">
+        <NavigationMenu className="m-5">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
+              <NavigationMenuContent className="min-w-[400px]">
+                <NavigationMenuLink className="mx-auto w-[350px] hover:bg-slate-50 py-10">Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Tools</NavigationMenuTrigger>
+              <NavigationMenuContent className="min-w-[400px]">
+                <NavigationMenuLink className="mx-auto w-[350px] hover:bg-slate-50 py-10">Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="m-5">
+          <LoginButton />
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="w-full flex justify-between" id="navbar">
+        <NavigationMenu className="m-5">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
+              <NavigationMenuContent className="min-w-[400px]">
+                <NavigationMenuLink className="hover:bg-slate-50">
+                  <a href="/" className="px-40">Resources</a>
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Tools</NavigationMenuTrigger>
+              <NavigationMenuContent className="min-w-[400px]">
+                <NavigationMenuLink className="mx-auto w-[350px] hover:bg-slate-50 py-10">Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="m-5">
+            <Avatar>
+              <AvatarImage src={session?.user?.image} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    )
+  }
+}
